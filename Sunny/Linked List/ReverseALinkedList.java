d10c96b9a971687b0162d2ace59229d897f8aa94
Sunny <sunny6464n@gmail.com>
2023-01-03 20:50:05 +0530

    Reverse a Linked List ✨

---
+++ Sunny/Linked List/ReverseALinkedList.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Node {
+    int value;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+class ReverseALinkedList{
+    public static void main(String[] args){
+        Node head = new Node(1); 
+        Node second = new Node(2); 
+        Node third = new Node(3); 
+        Node fourth = new Node(4); 
+        Node fifth = new Node(5); 
+        head.next = second; 
+        second.next = third; 
+        third.next = fourth; 
+        fourth.next = fifth; 
+        fifth.next = null;
+        head = reverseList(head);
+
+        display(head);
+
+    }
+    static Node reverseList(Node head)
+    {
+        // code here
+        Node next = head.next;
+        Node curr = head;
+       curr.next = null;
+       while(next != null){
+           Node temp = curr;
+           curr = next;
+           next = next.next;
+           curr.next = temp;
+        }
+        return curr; 
+    }
+
+
+    static void display(Node head){
+        Node ptr = head; 
+        while(ptr != null){
+            System.out.print(ptr.value + " -> "); 
+            ptr = ptr.next; 
+        }
+    }
+}
